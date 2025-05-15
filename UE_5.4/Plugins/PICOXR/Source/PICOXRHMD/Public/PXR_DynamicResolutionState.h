47f0ad07271d2188ea4f1544d59a1a7c8f95b4af
XinJin <xinjin@bytedance.com>
2025-05-15 15:50:52 +0800

    PICO Unreal Intergration SDK 3.1.0

---
+++ UE_5.4/Plugins/PICOXR/Source/PICOXRHMD/Public/PXR_DynamicResolutionState.h
@@ -0,0 +1,44 @@
+// Copyright 2015-2023 PICO Technology Co., Ltd. All rights reserved.
+// This plugin incorporates portions of the Unreal Engine. Unreal is a trademark or registered trademark of Epic Games, Inc. in the United States of America and elsewhere.
+// Unreal Engine, Copyright 1998  2023, Epic Games, Inc. All rights reserved. 
+
+#pragma once
+
+#include "PXR_HMD.h"
+#include "PXR_HMDSettings.h"
+#include "DynamicResolutionState.h"
+#include "DynamicResolutionProxy.h"
+
+//-------------------------------------------------------------------------------------------------
+// FPXR_DynamicResolutionState
+//-------------------------------------------------------------------------------------------------
+
+class FPXR_DynamicResolutionState : public IDynamicResolutionState
+{
+public:
+	FPXR_DynamicResolutionState(const FSettingsPtr InSettings, FPICOXRHMD* InHMD);
+	~FPXR_DynamicResolutionState() override;
+
+	// ISceneViewFamilyScreenPercentage
+	virtual void ResetHistory() override;
+	virtual bool IsSupported() const override;
+	virtual void SetupMainViewFamily(class FSceneViewFamily& ViewFamily) override;
+
+protected:
+	virtual DynamicRenderScaling::TMap<float> GetResolutionFractionsApproximation() const override;
+	virtual DynamicRenderScaling::TMap<float> GetResolutionFractionsUpperBound() const override;
+	virtual void SetEnabled(bool bEnable) override;
+	virtual bool IsEnabled() const override;
+	virtual void ProcessEvent(EDynamicResolutionStateEvent Event) override;
+	/** Apply the minimum/maximum resolution fraction for a third-party temporal upscaler. */
+	virtual void SetTemporalUpscaler(const UE::Renderer::Private::ITemporalUpscaler* InTemporalUpscaler)
+	{
+	}
+
+private:
+	const FSettingsPtr Settings;
+	FPICOXRHMD* HMD = nullptr;
+
+	float ResolutionFraction;
+	float ResolutionFractionUpperBound;
+};
